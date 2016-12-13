7cd233e88e7ab5ca07b5e1534a11606cfe0c82bd
Chattopadyay, Dipayan (DS) <Dipayan.Chattopadyay@cdk.com>
2016-12-12 19:39:22 -0800

    Some more progress

--- src/main/scala/com/persist/uw/examples/Cypher.scala
+++ src/main/scala/com/persist/uw/examples/Cypher.scala
@@ -24,7 +24,7 @@ class Graph(val nodes: Set[Node], val links: Set[Link]) {
 
   val nextRels: Map[Node, Set[Rel]] = links.groupBy(_.prev).map(e => e._1 -> e._2.map(_.r))
 
-  val nextNode: Map[Rel, Node] = links.groupBy(_.r).map(e => e._1 -> e._2.map(_.next).head)
+  val nextNode: Map[Rel, Node] = links.map({ case Link(r,n1,n2) => (r,n2) }).toMap
 
   def cypher[T: ClassTag, T1](pattern: Pattern[T])(act: (T) => T1): Set[T1] = {
     // could add optimization here
@@ -71,7 +71,7 @@ object Cypher {
   case class R[T <: Rel : ClassTag](cond: T => Boolean = (x: T) => true) extends RS[T] {
     def cypher(g: Graph, start: Rel): Set[T] = {
       val result = for(
-        p1 <- g.links.map(_.r).collect { case p: T => p}
+        p1 <- g.links.map(_.r).collect { case p: T if cond(p) => p}
       )yield p1
       result
     }
@@ -79,26 +79,37 @@ object Cypher {
 
   // compound pattern, node followed by node ...
   case class NN[T <: Node : ClassTag, T1: ClassTag](n: N[T], ns: NS[T1]) extends NS[(T, T1)] {
-    def cypher(g: Graph, start: Node): Set[(T, T1)] = ???
+    def cypher(g: Graph, start: Node): Set[(T, T1)] = {
+        for {
+        n1 <- n.cypher(g,start)
+        r1 <- g.nextRels.getOrElse(n1,Set())
+        n2 <- g.nextNode(r1)
+        n3 <- ns.cypher(g,n2)
+      } yield (n1,n3)
+    }
   }
 
   // compound pattern, node followed by rel ...
-  //Todo: Need help here
   case class NR[T <: Node : ClassTag, T1: ClassTag](n: N[T], rs: RS[T1]) extends NS[(T, T1)] {
     def cypher(g: Graph, start: Node): Set[(T, T1)] = {
-    /*  val result = for(
-        p1 <- g.nodes.collect {case p: T => p};
-        r1 <- g.nextRels.getOrElse(p1, Set()).collect { case b: RS[T1] => b };
-        p2 <- Seq(g.nextNode(r1)).collect { case m: T1 => m }
-      ) yield(p1,p2)
-      result */
-      ???
+      val result = for {
+        p1 <- n.cypher(g, start)
+        r1 <- g.nextRels.getOrElse(p1, Set())
+        r2 <- rs.cypher(g, r1)
+    } yield(p1,r2)
+      result
     }
   }
 
   // compound pattern, rel followed by node ...
   case class RN[T <: Rel : ClassTag, T1: ClassTag](r: R[T], ns: NS[T1]) extends RS[(T, T1)] {
-    def cypher(g: Graph, start: Rel): Set[(T, T1)] = ???
+    def cypher(g: Graph, start: Rel): Set[(T, T1)] = {
+      for {
+        r1 <- r.cypher(g, start)
+        n1 <- g.nextNode(r1)
+        n2 <- ns.cypher(g, n1)
+      } yield(r1,n2)
+    }
   }
 
   object --: {
